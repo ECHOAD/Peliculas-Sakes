3da16ef507d225c609037f3c36d17b309ef09d93
Adrian Estevez <adrianeea.ae@gmail.com>
2021-04-24 13:49:26 -0400

    Formulario Crear terminado V2

--- resources/views/trailers/_Form.blade.php
+++ resources/views/trailers/_Form.blade.php
@@ -1,20 +1,19 @@
 
 
-<div class="container ">
-    <div class="row">
-      <div id="contactdiv" class="container contactdiv">
-            <form class="form" action="#" id="contact">
+<!--Contact Form -->
+ <div id="contactdiv" class="contactdiv ">
+    <form class="form" action="#" id="contact">
 
+     
+      <h2 class="display-4">Crear nueva pelicula</h2>
+      
 
-
-
-
-
-                <input type="button" id="send" value="OK" />
-                <input type="button" id="cancel" value="Back" />
-                <br />
-            </form>
-      </div>
-    </div>
-</div>
+      
+      <input type="button" id="send" value="OK" />
+      <input type="button" id="cancel" value="Back" />
+      <br/>
+    </form>
+  </div>
+  <!-- jQuery Popup Form Example -->
+  <!-- ends here -->
 
---
+++ resources/views/trailers/create.blade.php
@@ -0,0 +1,8 @@
+@extends('Layout')
+
+
+@section('title','SAKE Peliculas | Crear Peliculas')
+
+@section('containt')
+
+@endsection
